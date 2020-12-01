c19e6f55d3ddb718cde90f7d3c4f4af7ba57d232
Chris VanHorn <chris_vnhrn@yahoo.com>
2020-12-01 12:43:12 -0600

    almost done in chapter 5

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('./app_server/models/db');
+
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 
